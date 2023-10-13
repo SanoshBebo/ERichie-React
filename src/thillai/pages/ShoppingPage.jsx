ae6a611bb6af546d9fc60975d6955d0c417f8cc9
SanoshBebo <sanoshbebo@gmail.com>
2023-10-13 09:55:56 +0530

    fixed latest code

--- src/thillai/pages/ShoppingPage.jsx
+++ src/thillai/pages/ShoppingPage.jsx
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
-import axios from 'axios';
-import ProductImg from '../components/shared/ProductImg';
-import './ShoppingPage.css';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+import React, { Component } from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import axios from "axios";
+import ProductImg from "../components/shared/ProductImg";
+import "./ShoppingPage.css";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import { Link } from 'react-router-dom';
 //import { useHistory } from 'react-router-dom';
-import { FaShoppingCart } from 'react-icons/fa';
-
+import { FaShoppingCart } from "react-icons/fa";
 
 class ShoppingPage extends Component {
   constructor(props) {
@@ -19,7 +17,7 @@ class ShoppingPage extends Component {
     this.state = {
       showProductModal: false,
       selectedProduct: null,
-      selectedCategory: 'All',
+      selectedCategory: "All",
       products: [], // Initialize products as an empty array
       cartCount: 0, // Initialize cart count
       quantity: 1, // Initialize quantity to 1
@@ -30,7 +28,7 @@ class ShoppingPage extends Component {
     };
   }
 
-  categories = ['All', 'Gaming'];
+  categories = ["All", "Gaming"];
 
   handleCloseProductModal = () => this.setState({ showProductModal: false });
 
@@ -51,7 +49,8 @@ class ShoppingPage extends Component {
 
   openProductModal = (product) => {
     const initialQuantity = 1; // Initial quantity is 1
-    const initialTotalPrice = (product.price?.integerValue || 0) * initialQuantity; // Calculate initial total price
+    const initialTotalPrice =
+      (product.price?.integerValue || 0) * initialQuantity; // Calculate initial total price
 
     this.setState({
       selectedProduct: product,
@@ -64,20 +63,21 @@ class ShoppingPage extends Component {
   openQuantityInputModal = () => {
     const { selectedProduct, quantity } = this.state;
 
-    const newQuantity = window.prompt('Enter quantity:', this.state.quantity);
+    const newQuantity = window.prompt("Enter quantity:", this.state.quantity);
     if (newQuantity !== null) {
       const parsedQuantity = parseInt(newQuantity, 10);
 
       if (!isNaN(parsedQuantity) && parsedQuantity > 0) {
         // Calculate the new total price
-        const newTotalPrice = selectedProduct.price?.integerValue * parsedQuantity;
+        const newTotalPrice =
+          selectedProduct.price?.integerValue * parsedQuantity;
 
         this.setState({
           quantity: parsedQuantity,
           totalPrice: newTotalPrice,
         });
       } else {
-        alert('Invalid quantity. Please enter a valid quantity.');
+        alert("Invalid quantity. Please enter a valid quantity.");
       }
     }
   };
@@ -86,20 +86,22 @@ class ShoppingPage extends Component {
     const { selectedProduct, quantity, totalPrice } = this.state;
 
     if (!selectedProduct) {
-      console.error('No product selected for purchase.');
+      console.error("No product selected for purchase.");
       return;
     }
 
     // Fetch the existing product data
     try {
-      const response = await axios.get(`https://firestore.googleapis.com/v1/projects/myapp-5dc30/databases/(default)/documents/Products/${selectedProduct.id}`);
+      const response = await axios.get(
+        `https://firestore.googleapis.com/v1/projects/myapp-5dc30/databases/(default)/documents/Products/${selectedProduct.id}`
+      );
       const existingProductData = response.data.fields;
 
       // Calculate the updated stock quantity
       const updatedQuantity = selectedProduct.stock?.integerValue - quantity;
 
       if (updatedQuantity < 0) {
-        console.error('Invalid quantity. Product quantity cannot be negative.');
+        console.error("Invalid quantity. Product quantity cannot be negative.");
         return;
       }
 
@@ -110,21 +112,27 @@ class ShoppingPage extends Component {
       };
 
       // Make a PATCH request to update the product document
-      await axios.patch(`https://firestore.googleapis.com/v1/projects/myapp-5dc30/databases/(default)/documents/Products/${selectedProduct.id}`, {
-        fields: updatedData,
-      });
+      await axios.patch(
+        `https://firestore.googleapis.com/v1/projects/myapp-5dc30/databases/(default)/documents/Products/${selectedProduct.id}`,
+        {
+          fields: updatedData,
+        }
+      );
 
       // Make a POST request to Firestore to create a new order
-      await axios.post('https://firestore.googleapis.com/v1/projects/myapp-5dc30/databases/(default)/documents/Orders', {
-        fields: {
-          Date: { stringValue: new Date().toISOString() },
-          ProductID: { stringValue: selectedProduct.id },
-          Quantity: { integerValue: quantity },
-          ShopID: { stringValue: 'shop07' },
-          TotalPrice: { doubleValue: totalPrice },
-          UserID: { stringValue: 'yourUserID' },
-        },
-      });
+      await axios.post(
+        "https://firestore.googleapis.com/v1/projects/myapp-5dc30/databases/(default)/documents/Orders",
+        {
+          fields: {
+            Date: { stringValue: new Date().toISOString() },
+            ProductID: { stringValue: selectedProduct.id },
+            Quantity: { integerValue: quantity },
+            ShopID: { stringValue: "shop07" },
+            TotalPrice: { doubleValue: totalPrice },
+            UserID: { stringValue: "yourUserID" },
+          },
+        }
+      );
 
       // If the requests succeed, you can reset the selectedProduct, quantity, and totalPrice
       this.setState({
@@ -134,14 +142,19 @@ class ShoppingPage extends Component {
         showSuccessModal: true, // Show the success modal
       });
     } catch (error) {
-      console.error('Error sending order:', error);
+      console.error("Error sending order:", error);
     }
   };
 
   handleFilter() {
     let filteredProducts = [];
-    if (this.state.selectedCategory !== 'All') {
-      filteredProducts = [...this.state.products.filter((product) => product.category?.stringValue === this.state.selectedCategory)];
+    if (this.state.selectedCategory !== "All") {
+      filteredProducts = [
+        ...this.state.products.filter(
+          (product) =>
+            product.category?.stringValue === this.state.selectedCategory
+        ),
+      ];
     } else {
       filteredProducts = [...this.state.products];
     }
@@ -152,14 +165,19 @@ class ShoppingPage extends Component {
   async componentDidMount() {
     try {
       // Fetch products from Firestore using Axios
-      const response = await axios.get('https://firestore.googleapis.com/v1/projects/myapp-5dc30/databases/(default)/documents/Products');
+      const response = await axios.get(
+        "https://firestore.googleapis.com/v1/projects/myapp-5dc30/databases/(default)/documents/Products"
+      );
 
       // Extract the data field from the Firestore response
-      const products = response.data.documents.map((doc) => ({ id: doc.name.split('/').pop(), ...doc.fields }));
+      const products = response.data.documents.map((doc) => ({
+        id: doc.name.split("/").pop(),
+        ...doc.fields,
+      }));
       console.log(products);
       this.setState({ products });
     } catch (error) {
-      console.error('Error fetching products:', error);
+      console.error("Error fetching products:", error);
     }
   }
 
@@ -172,17 +190,24 @@ class ShoppingPage extends Component {
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
     const filteredProducts = this.handleFilter();
-    const currentProducts = filteredProducts.slice(indexOfFirstItem, indexOfLastItem);
+    const currentProducts = filteredProducts.slice(
+      indexOfFirstItem,
+      indexOfLastItem
+    );
 
     // Calculate page numbers
     const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(filteredProducts.length / itemsPerPage); i++) {
+    for (
+      let i = 1;
+      i <= Math.ceil(filteredProducts.length / itemsPerPage);
+      i++
+    ) {
       pageNumbers.push(i);
     }
 
     return (
       <div>
-        <div className='navbar'>
+        <div className="navbar">
           <Link to="/erichie">Home Page</Link>
           <Link to="/gaming">Go back</Link>
           <Link to="/erichie/cart" className="navbar-button">
@@ -193,11 +218,14 @@ class ShoppingPage extends Component {
           </a>
         </div>
 
-        <div className='container d-flex justify-content-between border rounded p-2 shadow-sm align-items-center'>
-          <small className='text-muted d-none d-md-block'></small>
-          <div className='d-flex justify-content-between'>
-            <Dropdown onSelect={(value) => this.setState({ selectedCategory: value })} className='mx-1'>
-              <Dropdown.Toggle variant='secondary' id='dropdown-basic'>
+        <div className="container d-flex justify-content-between border rounded p-2 shadow-sm align-items-center">
+          <small className="text-muted d-none d-md-block"></small>
+          <div className="d-flex justify-content-between">
+            <Dropdown
+              onSelect={(value) => this.setState({ selectedCategory: value })}
+              className="mx-1"
+            >
+              <Dropdown.Toggle variant="secondary" id="dropdown-basic">
                 {this.state.selectedCategory}
               </Dropdown.Toggle>
               <Dropdown.Menu>
@@ -212,28 +240,51 @@ class ShoppingPage extends Component {
         </div>
 
         {this.state.showProductModal && this.state.selectedProduct && (
-          <div className='modal fade show' style={{ display: 'block' }}>
-            <div className='modal-dialog modal-dialog-centered modal-lg'>
-              <div className='modal-content'>
-                <div className='modal-header'>
-                  <h5 className='modal-title'>{this.state.selectedProduct.productname?.stringValue}</h5>
-                  <button type='button' className='btn-close' aria-label='Close' onClick={this.handleCloseProductModal}></button>
+          <div className="modal fade show" style={{ display: "block" }}>
+            <div className="modal-dialog modal-dialog-centered modal-lg">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">
+                    {this.state.selectedProduct.productname?.stringValue}
+                  </h5>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    aria-label="Close"
+                    onClick={this.handleCloseProductModal}
+                  ></button>
                 </div>
-                <div className='modal-body'>
-                  <div className='container'>
-                    <div className='row'>
-                      <div className='col-md-6'>
-                        <ProductImg src={this.state.selectedProduct.imageurl?.stringValue} alt='Product' className='img-fluid' />
+                <div className="modal-body">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-md-6">
+                        <ProductImg
+                          src={this.state.selectedProduct.imageurl?.stringValue}
+                          alt="Product"
+                          className="img-fluid"
+                        />
                       </div>
-                      <div className='col-md-6'>
-                        <p>Description: {this.state.selectedProduct.description?.stringValue}</p>
-                        <p>Price: Rs.{this.state.selectedProduct.price?.integerValue}</p>
+                      <div className="col-md-6">
+                        <p>
+                          Description:{" "}
+                          {this.state.selectedProduct.description?.stringValue}
+                        </p>
+                        <p>
+                          Price: Rs.
+                          {this.state.selectedProduct.price?.integerValue}
+                        </p>
                         <p>Quantity: {this.state.quantity}</p>
                         <p>Total Price: Rs.{this.state.totalPrice}</p>
-                        <button className='btn btn-primary' onClick={this.openQuantityInputModal}>
+                        <button
+                          className="btn btn-primary"
+                          onClick={this.openQuantityInputModal}
+                        >
                           Change Quantity
                         </button>
-                        <button className='btn btn-success' onClick={this.handlePurchase}>
+                        <button
+                          className="btn btn-success"
+                          onClick={this.handlePurchase}
+                        >
                           Purchase
                         </button>
                       </div>
@@ -245,23 +296,25 @@ class ShoppingPage extends Component {
           </div>
         )}
 
-        <div className='container border rounded mt-2 px-0 px-sm-2 shadow-sm'>
-          <div className='product-list'>
+        <div className="container border rounded mt-2 px-0 px-sm-2 shadow-sm">
+          <div className="product-list">
             {currentProducts.map((product, index) => {
               if (product) {
                 const category = product.category?.stringValue;
-                const description = product.description?.stringValue || product.descriprion?.stringValue; // Handle the typo
+                const description =
+                  product.description?.stringValue ||
+                  product.descriprion?.stringValue; // Handle the typo
                 const imageUrl = product.imageurl?.stringValue;
                 const price = product.price?.integerValue;
                 const productName = product.productname?.stringValue;
                 const stock = product.stock?.integerValue;
 
                 return (
-                  <div className='product-card' key={index}>
-                    <ProductImg src={imageUrl} alt='Product' />
-                    <p className='product-name'>{productName}</p>
-                    <p className='product-description'>{description}</p>
-                    <p className='product-price'>Price: Rs.{price}</p>
+                  <div className="product-card" key={index}>
+                    <ProductImg src={imageUrl} alt="Product" />
+                    <p className="product-name">{productName}</p>
+                    <p className="product-description">{description}</p>
+                    <p className="product-price">Price: Rs.{price}</p>
                     {this.openProductPage(product)}
                   </div>
                 );
@@ -272,25 +325,31 @@ class ShoppingPage extends Component {
           </div>
         </div>
 
-        <div className='pagination'>
+        <div className="pagination">
           {pageNumbers.map((number) => (
             <span
               key={number}
               onClick={() => this.handlePagination(number)}
-              className={number === currentPage ? 'active' : ''}
+              className={number === currentPage ? "active" : ""}
             >
               {number}
             </span>
           ))}
         </div>
 
-        <Modal show={this.state.showSuccessModal} onHide={() => this.setState({ showSuccessModal: false })}>
+        <Modal
+          show={this.state.showSuccessModal}
+          onHide={() => this.setState({ showSuccessModal: false })}
+        >
           <Modal.Header closeButton>
             <Modal.Title>Order Placed Successfully</Modal.Title>
           </Modal.Header>
           <Modal.Body>Your order has been successfully placed!</Modal.Body>
           <Modal.Footer>
-            <Button variant="success" onClick={() => this.setState({ showSuccessModal: false })}>
+            <Button
+              variant="success"
+              onClick={() => this.setState({ showSuccessModal: false })}
+            >
               Close
             </Button>
           </Modal.Footer>
