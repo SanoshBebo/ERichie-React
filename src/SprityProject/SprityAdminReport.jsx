da5859d885ad2889eaa9dfa9207be862086837b3
Sashaank Suresh <sashaank.suresh@psiog.com>
2023-10-16 15:27:43 +0530

    latest fix

--- src/SprityProject/SprityAdminReport.jsx
+++ src/SprityProject/SprityAdminReport.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
-import ReactPaginate from "react-paginate";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import {
   getOrderByDateFromFireStore,
   getOrderByDateRangeFromFireStore,
-} from "./GetOrderDetailSprity";
+} from "../SprityProject/GetOrderDetailSprity";
+import ReactPaginate from "react-paginate";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { setUser } from "../SanoshProject/redux/shopOneUserSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
 const SprityAdminReport = () => {
   const [orders, setOrders] = useState([]);
@@ -16,18 +19,40 @@ const SprityAdminReport = () => {
   const ordersPerPage = 7;
   const pageCount = Math.ceil(orders.length / ordersPerPage);
   const shopid = "shop10";
-
+  const [isDataLoaded, setisDataLoaded] = useState(false);
+  const user = useSelector((state) => state.shoponeuser.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [isLoadingUser, setIsLoadingUser] = useState(false);
   const handlePageClick = ({ selected }) => {
     setCurrentPage(selected);
   };
 
   const offset = currentPage * ordersPerPage;
   const currentPageData = orders.slice(offset, offset + ordersPerPage);
+
+  useEffect(() => {
+    if ((!isLoadingUser && user.length === 0) || user.role == "customer") {
+      navigate("/admin/login");
+    }
+  }, [isLoadingUser, user, navigate]);
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("user"));
+    if (userData) {
+      if (userData.role == "customer") {
+        navigate("/admin/login");
+      }
+      dispatch(setUser(userData));
+    }
+    setIsLoadingUser(false);
+  }, []);
   useEffect(() => {
     getOrderByDateFromFireStore(shopid)
       .then((todaysOrders) => {
         console.log(todaysOrders);
         setOrders(todaysOrders);
+        setisDataLoaded(true);
       })
       .catch((error) => {
         console.error("Error fetching product data:", error);
@@ -57,88 +82,59 @@ const SprityAdminReport = () => {
     <div className="min-h-screen p-10">
       <div className="min-h-[500px]">
         <h2 className="font-bold text-2xl mb-4">Today's Orders</h2>
-        <div className="overflow-x-auto">
-          <table className="w-full table-auto border-collapse">
-            <thead>
-              <tr className="bg-gray-100">
-                <th className="border-b border-gray-300 px-4 py-2">Order ID</th>
-                <th className="border-b border-gray-300 px-4 py-2">
-                  Customer Name
-                </th>
-                <th className="border-b border-gray-300 px-4 py-2">
-                  Purchase Date
-                </th>
-                <th className="border-b border-gray-300 px-4 py-2">
-                  Product Name
-                </th>
-                <th className="border-b border-gray-300 px-4 py-2">Quantity</th>
-                <th className="border-b border-gray-300 px-4 py-2">
-                  Current Stock
-                </th>
-                <th className="border-b border-gray-300 px-4 py-2">
-                  Total Price
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {currentPageData.map((order) => (
-                <tr key={order.orderid} className="hover:bg-gray-100">
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {order.orderid}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {order.name}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {order.purchaseDate.split("T")[0]}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {order.productname}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {order.quantity}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {order.currentstock}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {order.totalprice}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="pagination-container mt-4 flex justify-between">
-          <button
-            onClick={() => handlePageClick({ selected: currentPage - 1 })}
-            className={`${
-              currentPage === 0 ? "hidden" : "block"
-            } bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600`}
-          >
-            Previous
-          </button>
-          <ReactPaginate
-            previousLabel={""}
-            nextLabel={""}
-            pageCount={pageCount}
-            onPageChange={handlePageClick}
-            containerClassName={"pagination"}
-            pageLinkClassName={"hidden"}
-            previousLinkClassName={"hidden"}
-            nextLinkClassName={"hidden"}
-            disabledClassName={"pagination-disabled"}
-            activeClassName={"pagination-active"}
-          />
-          <button
-            onClick={() => handlePageClick({ selected: currentPage + 1 })}
-            className={`${
-              currentPage === pageCount - 1 ? "hidden" : "block"
-            } bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600`}
-          >
-            Next
-          </button>
-        </div>
+        {isDataLoaded &&
+          (currentPageData.length > 0 ? (
+            <div className="overflow-x-auto border border-gray-300">
+              <table className="w-full table-auto border-b border-gray-300">
+                <thead>
+                  <tr className="bg-gray-100">
+                    <th className="border-b border-gray-300 px-4 py-2">
+                      Order ID
+                    </th>
+                    <th className="border-b border-gray-300 px-4 py-2">
+                      Customer Name
+                    </th>
+                    <th className="border-b border-gray-300 px-4 py-2">
+                      Purchase Date
+                    </th>
+                    <th className="border-b border-gray-300 px-4 py-2">
+                      Product Name
+                    </th>
+                    <th className="border-b border-gray-300 px-4 py-2">
+                      Quantity
+                    </th>
+                    <th className="border-b border-gray-300 px-4 py-2">
+                      Current Stock
+                    </th>
+                    <th className="border-b border-gray-300 px-4 py-2">
+                      Total Price
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {currentPageData.map((order) => (
+                    <tr key={order.orderid} className="hover:bg-gray-100">
+                      <td className="px-4 py-2">{order.orderid}</td>
+                      <td className="px-4 py-2">{order.name}</td>
+                      <td className="px-4 py-2">
+                        {order.purchaseDate.split("T")[0]}
+                      </td>
+                      <td className="px-4 py-2">{order.productname}</td>
+                      <td className="px-4 py-2">{order.quantity}</td>
+                      <td className="px-4 py-2">{order.currentstock}</td>
+                      <td className="px-4 py-2">₹ {order.totalprice}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          ) : (
+            <div className="flex items-center justify-center text-center">
+              <h2 className="font-bold mt-44 text-2xl">
+                Oops Unfortunately no one has made any order for today 🙁
+              </h2>
+            </div>
+          ))}
       </div>
       <div className="flex-row space-y-10 ">
         <h2 className="font-bold text-2xl mb-4">Orders By Date</h2>
@@ -167,7 +163,7 @@ const SprityAdminReport = () => {
           </button>
         </div>
         <div className="overflow-x-auto">
-          <table className="w-full table-auto border-collapse">
+          <table className="w-full table-auto border border-gray-300">
             {ordersByDate.length > 0 ? (
               <thead>
                 <tr className="bg-gray-100">
@@ -198,24 +194,14 @@ const SprityAdminReport = () => {
             <tbody>
               {ordersByDate.map((order) => (
                 <tr key={order.orderid} className="hover:bg-gray-100">
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {order.orderid}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {order.name}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
+                  <td className=" px-4 py-2">{order.orderid}</td>
+                  <td className=" px-4 py-2">{order.name}</td>
+                  <td className="px-4 py-2">
                     {order.purchaseDate.split("T")[0]}
                   </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {order.productname}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {order.quantity}
-                  </td>
-                  <td className="border-b border-gray-300 px-4 py-2">
-                    {order.totalprice}
-                  </td>
+                  <td className="px-4 py-2">{order.productname}</td>
+                  <td className=" px-4 py-2">{order.quantity}</td>
+                  <td className=" px-4 py-2">₹ {order.totalprice}</td>
                 </tr>
               ))}
             </tbody>
